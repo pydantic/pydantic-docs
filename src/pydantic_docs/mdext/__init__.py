6f657b30593b00cf5f7c0b7eca74e83b3b6b7819
Victorien <65306057+Viicos@users.noreply.github.com>
2025-11-21 11:58:01 +0100

    Add version change blocks (#3)

--- src/pydantic_docs/mdext/__init__.py
+++ src/pydantic_docs/mdext/__init__.py
@@ -4,11 +4,16 @@ from markdown import Markdown
 from pymdownx.blocks import BlocksExtension, BlocksProcessor
 
 from .public_trace import PublicTrace
+from .versioning import VersionAdded, VersionChanged, VersionDeprecated, VersionDeprecatedRemoved
 
 
 class PydanticDocsExtension(BlocksExtension):
     def extendMarkdownBlocks(self, md: Markdown, block_mgr: BlocksProcessor) -> None:
         block_mgr.register(PublicTrace, self.getConfigs())
+        block_mgr.register(VersionAdded, self.getConfigs())
+        block_mgr.register(VersionChanged, self.getConfigs())
+        block_mgr.register(VersionDeprecated, self.getConfigs())
+        block_mgr.register(VersionDeprecatedRemoved, self.getConfigs())
 
 
 def makeExtension(*args: Any, **kwargs: Any) -> PydanticDocsExtension:
